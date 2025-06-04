59d135ba49d4efb2992df06452c75c064e7b6ad0
affandi-cetta <affandi@cetta.tech>
2025-06-04 16:02:02 +0700

    Add Swagger Docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('The API documentation for my project')
+    .setVersion('1.0')
+    .addBearerAuth() // If using JWT auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: http://localhost:${process.env.PORT}`);
+  console.log(
+    `Application is running on: http://localhost:${process.env.PORT}`,
+  );
 }
 bootstrap();
