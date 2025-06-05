7b2965d2444995332bc761c6a6223787d72fc7d7
affandi-cetta <affandi@cetta.tech>
2025-06-05 09:27:59 +0700

    adding User DTO

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+// src/user/dto/create-user.dto.ts
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  role?: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,39 +1,44 @@
 // src/user/user.controller.ts
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
-  async findAll(): Promise<User[]> {
+  findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<User | null> {
+  findOne(@Param('id') id: string): Promise<User | null> {
     return this.userService.findOne(id);
   }
 
   @Post()
-  async create(
-    @Body() body: { name: string; email: string; password: string; role?: string }
-  ): Promise<User> {
+  create(@Body() body: CreateUserDto): Promise<User> {
     return this.userService.create(body);
   }
 
   @Put(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() body: Partial<{ name: string; email: string; password: string; role: string }>
-  ): Promise<User> {
+  update(@Param('id') id: string, @Body() body: UpdateUserDto): Promise<User> {
     return this.userService.update(id, body);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string): Promise<User> {
+  remove(@Param('id') id: string): Promise<User> {
     return this.userService.remove(id);
   }
 }
