f7da44ebac8d1be96bdf921b81c5c08d5d07c843
affandi-cetta <affandi@cetta.tech>
2025-06-05 14:44:43 +0700

    Update Log Main

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,14 @@ async function bootstrap() {
     .setTitle('My API')
     .setDescription('The API documentation for my project')
     .setVersion('1.0')
-    .addBearerAuth() 
+    .addBearerAuth()
     .build();
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, 
-      forbidNonWhitelisted: true, 
-      transform: true, 
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
   const api = 'swagger';
@@ -24,8 +24,22 @@ async function bootstrap() {
   SwaggerModule.setup(api, app, document);
 
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`
+    ▓█████▄  ▄▄▄       ██▓ ██▓   ▓██   ██▓   ▄▄▄█████▓ ██▀███   ▄▄▄       ▄████▄   ██ ▄█▀▓█████  ██▀███  
+    ▒██▀ ██▌▒████▄    ▓██▒▓██▒    ▒██  ██▒   ▓  ██▒ ▓▒▓██ ▒ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒
+    ░██   █▌▒██  ▀█▄  ▒██▒▒██░     ▒██ ██░   ▒ ▓██░ ▒░▓██ ░▄█ ▒▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒
+    ░▓█▄   ▌░██▄▄▄▄██ ░██░▒██░     ░ ▐██▓░   ░ ▓██▓ ░ ▒██▀▀█▄  ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  
+    ░▒████▓  ▓█   ▓██▒░██░░██████▒ ░ ██▒▓░     ▒██▒ ░ ░██▓ ▒██▒ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒
+    ▒▒▓  ▒  ▒▒   ▓▒█░░▓  ░ ▒░▓  ░  ██▒▒▒      ▒ ░░   ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░
+    ░ ▒  ▒   ▒   ▒▒ ░ ▒ ░░ ░ ▒  ░▓██ ░▒░        ░      ░▒ ░ ▒░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░
+    ░ ░  ░   ░   ▒    ▒ ░  ░ ░   ▒ ▒ ░░       ░        ░░   ░   ░   ▒   ░        ░ ░░ ░    ░     ░░   ░ 
+      ░          ░  ░ ░      ░  ░░ ░                    ░           ░  ░░ ░      ░  ░      ░  ░   ░                            
+                                                                                        
+    `);
+
   console.log(
-    `Application is running on: http://localhost:${process.env.PORT} ] \nSwagger documentation is on http://localhost:${process.env.PORT}/${api} `,
+    `Application is running on: http://localhost:${process.env.PORT} ]
+Swagger documentation is on http://localhost:${process.env.PORT}/${api} `,
   );
 }
 bootstrap();
