056eb9dab267c63122da85e613e876b8c1e01017
affandi-cetta <affandi@cetta.tech>
2025-07-06 00:48:35 +0700

    FEAT : Selected Return Data

--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -2,41 +2,67 @@
 import { Injectable, ForbiddenException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateMemberDto } from './dto/create-member.dto';
+import {
+  throwConflict,
+  throwForbidden,
+  throwNotFound,
+} from 'src/custom/helper/http.response';
+import { memberSelect } from 'src/prisma/select/member';
 
 @Injectable()
 export class MemberService {
   constructor(private prisma: PrismaService) {}
 
   async create(userId: string, dto: CreateMemberDto) {
+    const existing = await this.prisma.member.findUnique({
+      where: { name: dto.name },
+    });
+
+    if (existing) {
+      throwConflict('Member dengan nama tersebut sudah ada');
+    }
     return this.prisma.member.create({
       data: {
         name: dto.name,
         userId,
       },
+      select: memberSelect,
     });
   }
 
   async findAll(userId: string) {
     return this.prisma.member.findMany({
       where: { userId },
+      select: memberSelect,
     });
   }
 
   async remove(userId: string, id: string) {
-    const member = await this.prisma.member.findUnique({ where: { id } });
-    if (!member || member.userId !== userId) {
-      throw new ForbiddenException('Akses ditolak');
+    const member = await this.prisma.member.findUnique({
+      where: { id, isDeleted: false },
+    });
+
+    if (!member) {
+      throwNotFound('Member tidak ditemukan');
+    }
+    if (member.userId !== userId) {
+      throwForbidden('Akses ditolak');
     }
 
-    // Optional: Cek apakah ada relasi progress/target
     const usedInProgress = await this.prisma.progress.findFirst({
       where: { memberId: id },
     });
 
     if (usedInProgress) {
-      throw new ForbiddenException('Member sudah memiliki progress, tidak bisa dihapus');
+      throw new ForbiddenException(
+        'Member sudah memiliki progress, tidak bisa dihapus',
+      );
     }
 
-    return this.prisma.member.delete({ where: { id } });
+    return this.prisma.member.update({
+      where: { id },
+      data: { isDeleted: true },
+      select: { name: true },
+    });
   }
 }
