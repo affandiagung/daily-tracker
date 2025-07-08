91f3073c616fe20b16832a0c47f68b3140209d4d
affandi-cetta <affandi@cetta.tech>
2025-07-08 15:30:26 +0700

    FEAT: Using Docker Image

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(api, app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   console.log(
     `\nServer in mode ${process.env.NODE_ENV === 'test' ? 'TESTING' : 'PRODUCTION'} `,
   );
